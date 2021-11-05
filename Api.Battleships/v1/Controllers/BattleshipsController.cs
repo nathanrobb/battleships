00a9587943088cdb1a475ceaf04bb75a112f41db
nathanrobb <nathanrobb@hotmail.com>
2021-11-06 11:32:34 +1300

    Add PATCH v1/battleships/fire-torpedo
    - This endpoint will control the game turns as the user enters coordinates to fire at.
    - Returns the torpedo result with game state info that the front-end can use to display info to the user

--- Api.Battleships/v1/Controllers/BattleshipsController.cs
+++ Api.Battleships/v1/Controllers/BattleshipsController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 
 namespace Api.Battleships.v1.Controllers
 {
@@ -10,6 +11,13 @@ namespace Api.Battleships.v1.Controllers
 	[ApiController]
 	public class BattleshipsController : ControllerBase
 	{
+		private readonly ILogger<BattleshipsController> _logger;
+
+		public BattleshipsController(ILogger<BattleshipsController> logger)
+		{
+			_logger = logger;
+		}
+
 		[AllowAnonymous]
 		[HttpPost("new-game")]
 		[ProducesResponseType(typeof(NewGameResponse), StatusCodes.Status200OK)]
@@ -21,6 +29,27 @@ namespace Api.Battleships.v1.Controllers
 			{
 				Guesses = 20,
 				Ships = 2,
+			});
+		}
+
+		[AllowAnonymous]
+		[HttpPatch("fire-torpedo")]
+		[ProducesResponseType(typeof(FireTorpedoResponse), StatusCodes.Status200OK)]
+		public IActionResult PatchFireTorpedo([FromBody] FireTorpedoRequest request)
+		{
+			if (request == null || string.IsNullOrWhiteSpace(request.Coordinate))
+				return BadRequest("Must specify coordinates in request body");
+
+			_logger.LogDebug($"Fired torpedo at {request.Coordinate}");
+
+			// TODO: actually fire the torpedo
+
+			return Ok(new FireTorpedoResponse
+			{
+				GuessesRemaining = 20,
+				ShipsRemaining = 2,
+				ShipProximity = "",
+				ShipSunk = false,
 			});
 		}
 	}
---
+++ Api.Battleships/v1/Models/FireTorpedoResponse.cs
@@ -0,0 +1,31 @@
+﻿namespace Api.Battleships.v1.Models
+{
+	public class FireTorpedoResponse
+	{
+		/// <summary>
+		/// The number of guesses remaining to sink all the ships.
+		/// </summary>
+		/// <remarks>When this reaches 0 (and at least 1 ship remains), the game is lost.</remarks>
+		public int GuessesRemaining { get; set; }
+		/// <summary>
+		/// The number of ships remaining to sink.
+		/// </summary>
+		/// <remarks>When this reaches 0, the game is won.</remarks>
+		public int ShipsRemaining { get; set; }
+		/// <summary>
+		/// The proximity of the fired torpedo, one of either: hit, hot, warm, cold.
+		/// </summary>
+		/// <remarks>
+		/// Hit is when a ship cell is hit.
+		/// Hot is when a ship cell is within 1-2 cells.
+		/// Warm is when a ship cell is within 3-4 cells.
+		/// Cold is when a ship cell is greater than 4 cells away.
+		/// </remarks>
+		// TODO: return an enum/number instead?
+		public string ShipProximity { get; set; }
+		/// <summary>
+		/// When true, the previously fired torpedo hit and sunk a ship.
+		/// </summary>
+		public bool ShipSunk { get; set; }
+	}
+}
