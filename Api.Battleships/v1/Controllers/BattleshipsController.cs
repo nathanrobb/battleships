1af70bb79cac0eff3806f0457fc98be988e89193
nathanrobb <nathanrobb@hotmail.com>
2021-11-06 11:29:14 +1300

    Add POST v1/battleships/new-game
    - This endpoint will be responsible for creating a new game of battleship so the end user can start or restart a game.
    - Returns general game info on ship count and how many guesses the user has to sink the ships

---
+++ Api.Battleships/v1/Controllers/BattleshipsController.cs
@@ -0,0 +1,27 @@
+﻿using Api.Battleships.v1.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Battleships.v1.Controllers
+{
+	[Route("v1/[controller]")]
+	[Authorize]
+	[ApiController]
+	public class BattleshipsController : ControllerBase
+	{
+		[AllowAnonymous]
+		[HttpPost("new-game")]
+		[ProducesResponseType(typeof(NewGameResponse), StatusCodes.Status200OK)]
+		public IActionResult PostNewGame()
+		{
+			// TODO: actually start a new game
+
+			return Ok(new NewGameResponse
+			{
+				Guesses = 20,
+				Ships = 2,
+			});
+		}
+	}
+}
---
+++ Api.Battleships/v1/Models/NewGameResponse.cs
@@ -0,0 +1,14 @@
+﻿namespace Api.Battleships.v1.Models
+{
+	public class NewGameResponse
+	{
+		/// <summary>
+		/// The number of guesses available to sink all the ships.
+		/// </summary>
+		public int Guesses { get; set; }
+		/// <summary>
+		/// The number of ships to sink.
+		/// </summary>x
+		public int Ships { get; set; }
+	}
+}
