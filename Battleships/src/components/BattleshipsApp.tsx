ee0f1609652b287b7999b19d1417c39d0a745ad9
nathanrobb <nathanrobb@hotmail.com>
2021-11-07 02:17:39 +1300

    WIP setup the main app app component
    - This has some rough state handling to get things going, we'll look at adding a context in later.

---
+++ Battleships/src/components/BattleshipsApp.tsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { FireTorpedoButton } from './FireTorpedoButton';
+import { FiredTorpedoMessage } from './FiredTorpedoMessage';
+import { GameState } from './GameState';
+import { Heading } from './Heading';
+import { NewGameButton } from './NewGameButton';
+import { CoordinateInput } from './CoordinateInput';
+import { FiredTorpedoResult } from '../types/FiredTorpedoResult';
+import { Game } from '../types/Game';
+
+const BattleshipsApp: React.FC = () => {
+  // Add a context/reducer
+  const [gameId, setGameId] = useState<number | undefined>();
+  const [boardSize, setBoardSize] = useState<number | undefined>();
+  const [guesses, setGuesses] = useState<number | undefined>();
+  const [ships, setShips] = useState<number | undefined>();
+
+  const [row, setRow] = useState<number | undefined>();
+  const [column, setColumn] = useState<number | undefined>();
+
+  const [shipSunk, setShipSunk] = useState<boolean>(false);
+  const [distance, setDistance] = useState<number | undefined>();
+
+  const onNewGame = (game: Game) => {
+    setGameId(game.gameId);
+    setBoardSize(game.boardSize);
+    setGuesses(game.guessesRemaining);
+    setShips(game.shipsRemaining);
+
+    setRow(undefined);
+    setColumn(undefined);
+
+    setShipSunk(false);
+    setDistance(undefined);
+  };
+
+  const onFiredTorpedo = (firedTorpedo: FiredTorpedoResult) => {
+    setGuesses(firedTorpedo.guessesRemaining);
+    setShips(firedTorpedo.shipsRemaining);
+
+    setRow(undefined);
+    setColumn(undefined);
+
+    setShipSunk(firedTorpedo.shipSunk);
+    setDistance(firedTorpedo.distance);
+  };
+
+  const onCoordinateInput = (row: number, column: number) => {
+    setRow(row);
+    setColumn(column);
+  };
+
+  return (
+    <div>
+      <Heading gameId={gameId} />
+      {gameId && boardSize && guesses && ships && (
+        <>
+          <CoordinateInput
+            boardSize={boardSize}
+            onValidBlur={onCoordinateInput}
+          />
+          <FireTorpedoButton
+            gameId={gameId}
+            row={row}
+            column={column}
+            onFiredTorpedo={onFiredTorpedo}
+          />
+          {distance && (
+            <FiredTorpedoMessage
+              guesses={guesses}
+              ships={ships}
+              shipSunk={shipSunk}
+              distance={distance}
+            />
+          )}
+          <GameState guesses={guesses} ships={ships} />
+        </>
+      )}
+      <NewGameButton onNewGame={onNewGame} />
+    </div>
+  );
+};
+
+export { BattleshipsApp };
