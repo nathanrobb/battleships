3537fa3eacca5947bcb0ad0bb6499492d57f6903
nathanrobb <nathanrobb@hotmail.com>
2021-11-07 10:30:26 +1300

    Add ShipDistanceService to find the min ship distance from a torpedo

---
+++ Api.Battleships.Tests/Services/ShipDistanceServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using Api.Battleships.Database.Models;
+using Api.Battleships.Services;
+using Api.Battleships.Services.Models;
+using NUnit.Framework;
+
+namespace Api.Battleships.Tests.Services
+{
+	[TestFixture]
+	public class ShipDistanceServiceTests
+	{
+		private ShipDistanceService _shipDistanceService;
+
+		[SetUp]
+		public void SetUp()
+		{
+			 _shipDistanceService = new ShipDistanceService();
+		}
+
+		[Test]
+		public void GetClosestShipCell_MultipleCells_ReturnsClosesCell()
+		{
+			// Arrange
+			var torpedo = new Coordinate(3, 5);
+			var shipCells = new []
+			{
+				new ShipCell { Row = 2, Column = 7 },
+				new ShipCell { Row = 2, Column = 8 },
+			};
+
+			// Act
+			var actual = _shipDistanceService.GetClosestShipCell(torpedo, shipCells);
+
+			// Assert
+			Assert.IsNotNull(actual);
+			Assert.AreEqual(shipCells[0], actual.ShipCell);
+			Assert.AreEqual(3, actual.Distance);
+		}
+
+		[Test]
+		public void GetClosestShipCell_NoCells_ReturnsNull()
+		{
+			// Arrange
+			var torpedo = new Coordinate(1, 1);
+			var shipCells = new ShipCell[0];
+
+			// Act
+			var actual = _shipDistanceService.GetClosestShipCell(torpedo, shipCells);
+
+			// Assert
+			Assert.IsNull(actual);
+		}
+	}
+}
---
+++ Api.Battleships/Services/Models/TorpedoDistance.cs
@@ -0,0 +1,22 @@
+﻿using Api.Battleships.Database.Models;
+
+namespace Api.Battleships.Services.Models
+{
+	public class TorpedoDistance
+	{
+		/// <summary>
+		/// The ship cell that the distance was calculated from.
+		/// </summary>
+		public ShipCell ShipCell { get; }
+		/// <summary>
+		/// The distance the torpedo was from the ship cell.
+		/// </summary>
+		public int Distance { get; }
+
+		public TorpedoDistance(ShipCell shipCell, int distance)
+		{
+			ShipCell = shipCell;
+			Distance = distance;
+		}
+	}
+}
